7c623a17cd452e8156b0cd24e5efcd9f29cf1e31
s24377 <s24377@pjwstk.edu.pl>
2024-11-23 17:42:12 +0100

    feat: add category endpoints
    REF: TASK-91
    
    Took 16 minutes

---
+++ src/main/java/com/dama/wanderwave/categoryType/CategoryTypeResponse.java
@@ -0,0 +1,7 @@
+package com.dama.wanderwave.categoryType;
+
+public class CategoryTypeResponse {
+	private String id;
+	private String name;
+	private String imageUrl;
+}
---
+++ src/main/java/com/dama/wanderwave/categoryType/CategoryTypeService.java
@@ -0,0 +1,48 @@
+package com.dama.wanderwave.categoryType;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CategoryTypeService {
+
+	private final CategoryTypeRepository categoryTypeRepository;
+	private final ModelMapper modelMapper;
+
+	public CategoryTypeResponse getCategoryTypeById(String id) {
+		log.info("Fetching CategoryType with ID: {}", id);
+
+		CategoryType categoryType = categoryTypeRepository.findById(id)
+				                            .orElseThrow(() -> {
+					                            log.error("CategoryType with ID: {} not found", id);
+					                            return new EntityNotFoundException("CategoryType not found with ID: " + id);
+				                            });
+
+
+		CategoryTypeResponse response = modelMapper.map(categoryType, CategoryTypeResponse.class);
+		log.info("Successfully fetched and mapped CategoryType with ID: {}", id);
+		return response;
+	}
+
+	public List<CategoryTypeResponse> getAllCategoryTypes() {
+		log.info("Fetching all CategoryTypes");
+
+		List<CategoryType> categoryTypes = categoryTypeRepository.findAll();
+
+		List<CategoryTypeResponse> responses = categoryTypes.stream()
+				                                       .map(categoryType -> modelMapper.map(categoryType, CategoryTypeResponse.class))
+				                                       .toList();
+
+		log.info("Successfully fetched and mapped {} CategoryTypes", responses.size());
+		return responses;
+	}
+
+}
