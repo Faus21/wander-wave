42696a6b4ec02e8c14d7aacb577aee77ff783dd6
Daniil Pavlovskyi <daniil.pavlovskyi@gmail.com>
2024-12-27 22:08:49 +0100

    feat: added hashtag controller/service

---
+++ src/main/java/com/dama/wanderwave/hashtag/HashTagController.java
@@ -0,0 +1,41 @@
+package com.dama.wanderwave.hashtag;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Max;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hashtags")
+@RequiredArgsConstructor
+@Tag(name = "Hashtags", description = "Endpoints for managing and retrieving hashtags")
+public class HashTagController {
+
+    private final static int MAX_PAGE_SIZE = 50;
+    private final HashTagService hashTagService;
+
+    @GetMapping
+    @Operation(summary = "Retrieve all hashtags", description = "Retrieves all hashtags in a paginated format.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Hashtags retrieved successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<Page<HashTag>> getAllHashTags(
+            @RequestParam @Max(MAX_PAGE_SIZE) Integer page,
+            @RequestParam int size) {
+        Page<HashTag> hashTags = hashTagService.getAllHashTags(PageRequest.of(page, size));
+        return new ResponseEntity<>(hashTags, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/dama/wanderwave/hashtag/HashTagService.java
@@ -0,0 +1,18 @@
+package com.dama.wanderwave.hashtag;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class HashTagService {
+
+    private final HashTagRepository hashTagRepository;
+
+    public Page<HashTag> getAllHashTags(Pageable pageable) {
+        return hashTagRepository.findAll(pageable);
+    }
+}
